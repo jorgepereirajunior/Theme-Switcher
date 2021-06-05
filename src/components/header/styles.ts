720e364ee9061ad3ae5206bbbc284c6720358ae1
Jorginho <jorgejunior.jpj2016@gmail.com>
2021-06-05 00:57:05 -0300

    Cria estilo do component Header

---
+++ src/components/header/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 50px;
+  width: 100%;
+
+  background-color: tomato;
+  box-shadow: 1px 1px 5px rgba(0,0,0,0.6);
+`
+
+export const Title = styled.h1`
+  font-size: 21px;
+  font-weight: bold;
+  font-family: sans-serif;
+`
