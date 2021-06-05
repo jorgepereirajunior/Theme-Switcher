ee9c90989f1893289f571ceaa6c1b3d6445ce0aa
Jorginho <jorgejunior.jpj2016@gmail.com>
2021-06-05 17:59:33 -0300

    Cria tema dark

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  name: 'dark',
+
+  colors: {
+    primary: '#071013',
+
+    switch: '#ff6347',
+    title: '#ffffee',
+    background: '#2a2d34'
+  }
+}
